12eec80bf1057d3a144c994c70f1a00fcf3dd722
Priyanshu Anand <anandpriyanshu6@gmail.com>
2024-08-10 19:32:05 +0530

    Complete Login Registration Form Using Template Driven Approach

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  // Below will be used to store the actual 2 way binded data
+  loginData = {
+    email: '',
+    password: '',
+    isManager: 'false',
+  };
 
+  // Below will be used to provide initial values to the form again after the reset of form is done
+  loginDataInitialValues = {
+    email: '',
+    password: '',
+    isManager: 'false',
+  };
+
+  handleLogin(loginForm: NgForm) {
+    console.log(this.loginData);
+  }
+
+  handleReset(loginForm: NgForm) {
+    loginForm.reset(this.loginDataInitialValues);
+  }
 }
