02d1d9fee9c34f78dffc840e59505c960f252725
Priyanshu Anand <anandpriyanshu6@gmail.com>
2024-12-29 05:27:14 +0530

    Updated File Structure With Renamed Components

---
+++ src/app/components/dashboard-tabs/dashboard-tabs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-tabs',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './dashboard-tabs.component.html',
+  styleUrl: './dashboard-tabs.component.css'
+})
+export class DashboardTabsComponent {
+
+}
